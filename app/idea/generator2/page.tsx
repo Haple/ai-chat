ae828323173ccd52c72724014fc9cad73423fc96
aleph <aleph@papayapay.com>
2024-01-07 20:35:35 -0300

    Fix IdeGeneratorPage2

--- app/idea/generator2/page.tsx
+++ app/idea/generator2/page.tsx
@@ -5,20 +5,10 @@ import React, { useState } from 'react';
 import { IdeaGenerator } from '@/components/idea-generator2'
 
 import { auth } from '@/auth'
-import { runPipelineStep } from './actions';
 
-export interface IdeaGeneratorPageProps extends React.ComponentProps<'div'> {
-
-  searchParams:{
-    pipelineId?: string,
-    currentStep: string,
-  }
-}
-
-export default async function IdeaGeneratorPage({ searchParams:{pipelineId, currentStep = '0'}, children }: IdeaGeneratorPageProps) {
+export default async function IdeaGeneratorPage() {
 
   const session = await auth();
-  const parsedCurrentStep = parseInt(currentStep);
 
   if (!session?.user) {
     redirect(`/sign-in?next=/idea/generator`);
